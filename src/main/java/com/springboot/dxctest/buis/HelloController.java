aaa577ebb1d207c2b2efbdd53eec385afd65f030
jluo4 <junxiang.luo@dxc.com>
2022-10-28 15:29:35 +0800

    11

--- src/main/java/com/springboot/dxctest/buis/HelloController.java
+++ src/main/java/com/springboot/dxctest/buis/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.dxctest.buis.dao.CustomerInfoDao;
+import com.springboot.dxctest.buis.redis.RedisUtils;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -15,12 +16,31 @@ public class HelloController {
 	
 	  @Autowired
 	  private CustomerInfoDao customerInfoDao;
+	  
+	  @Autowired
+	    private RedisUtils redisUtils;
 
     @RequestMapping("hello")
     String home(HttpServletRequest request) {
         int count = 105;
         CustomerInfoEntity c =  customerInfoDao.selectById(1);
         return "欢迎您" +  c.getUserName() + "，这是您的手机号码是  " + c.getUserMobile();
+    }
+    
+    @RequestMapping("hello2")
+    String home2(HttpServletRequest request) {
+        int count = 105;
+        if (redisUtils.get("test") == null ) {
+        	System.out.println("nofind value in reids");
+        	
+        	redisUtils.set("test", "asdasdasdasd");
+            return "nofind value in reids您" ;
+
+        } else {
+        	System.out.println(redisUtils.get("test"));
+            return "find value in reids " + redisUtils.get("test") ;
+        	
+        }
     }
 
 }
